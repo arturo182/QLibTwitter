53d1092ef9116b160cd1a23c366f27d8332f5ac3
arturo182 <arturo182@tlen.pl>
2010-08-20 11:46:58 +0200

    Fixed Parser::getStatusList bug (first Tweet not being added to list).

--- src/QLibTwitterParser.cpp
+++ src/QLibTwitterParser.cpp
@@ -135,7 +135,7 @@ QList<elStatus*> Parser::getStatusList(QString xml)
   dom.setContent(xml);
 
   QDomNodeList nodes = dom.elementsByTagName("status");
-  for(int i = nodes.count()-1; i > 0; i--) {
+  for(int i = nodes.count()-1; i >= 0; i--) {
     elStatus *el = new elStatus();
 
     QDomNode status = nodes.at(i);
