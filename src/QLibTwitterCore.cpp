20d3d46aa51f3f69efdd8a26ec8cb917b1c6a03e
arturo182 <arturo182@tlen.pl>
2010-08-21 02:08:27 +0200

    Changed copyright blocks to GPL info

--- src/QLibTwitterCore.cpp
+++ src/QLibTwitterCore.cpp
@@ -1,8 +1,19 @@
-/**
-  * QLibTwitter
-  * Copyright (c) 2010 arturo182 <arturo182@tlen.pl>
-  * All rights reserved
-  */
+/************************************************************************
+ * QLibTwitter                                                          *
+ * Copyright (C) 2010 arturo182 <arturo182@tlen.pl>                     *
+ *                                                                      *
+ * This library is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; version 3 only.                        *
+ *                                                                      *
+ * This library is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this library. If not, see <http://www.gnu.org/licenses/>. *
+ ************************************************************************/
 
 #include "QLibTwitterCore.h"
 #include "QLibTwitterParser.h"
